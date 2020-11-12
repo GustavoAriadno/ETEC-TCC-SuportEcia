0d68725b68c28c4c5af591e395484e355ea9ba53
GustavoAriadno <gugu.gustavo2011@gmail.com>
2020-11-12 03:04:35 -0300

    Implement otp check

--- SuportEtecia/app/src/main/java/com/example/suportetecia/InsertPassCode.java
+++ SuportEtecia/app/src/main/java/com/example/suportetecia/InsertPassCode.java
@@ -31,11 +31,10 @@ public class InsertPassCode extends AppCompatActivity {
 
     EditText editText;
     Button scanBtn;
-    TextView textView;
     private RequestQueue mQueue;
 
-    String fullHash;
     String userEmail;
+    String fullHash;
     String otp;
 
     @Override
@@ -47,13 +46,11 @@ public class InsertPassCode extends AppCompatActivity {
         fullHash = getIntent().getStringExtra("FULL_HASH");
         userEmail = getIntent().getStringExtra("USER_EMAIL");
 
-        textView = findViewById(R.id.tvShow);
         editText = findViewById(R.id.txtPassCode);
         scanBtn = findViewById(R.id.btnScan);
         scanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                textView.setText(fullHash);
                 otp = editText.getText().toString().trim();
 //              Check OTP
                 checkOtp(otp, userEmail, fullHash);
@@ -76,8 +73,7 @@ public class InsertPassCode extends AppCompatActivity {
                     try {
                         preStartScan(response.getInt("status"));
                     } catch (JSONException e) {
-                        textView.append("n funfou ");
-                        Toast.makeText(getApplicationContext(), "n funfo", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Problemas na requisição", Toast.LENGTH_SHORT).show();
                         e.printStackTrace();
                     }
                 }
@@ -115,27 +111,94 @@ public class InsertPassCode extends AppCompatActivity {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() != null) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage(result.getContents());
-                builder.setTitle("Titulo");
-                builder.setPositiveButton("Scan Again", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        scanCode();
-                    }
-                }).setNegativeButton("Finish", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                parseRetCode(result.getContents());
             } else {
-                Toast.makeText(this, "No Results", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Sem resultados", Toast.LENGTH_LONG).show();
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    private void parseRetCode(String retCode) {
+        String url = "https://suportecia.herokuapp.com/public/equipamentos/sigla";
+//      Define request body
+        Map<String, String> reqBody = new HashMap<String, String>();
+        reqBody.put("sigla", retCode);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(reqBody),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String local = null;
+                            String sigla = null;
+                            if (response.getInt("status") == 1){
+                                local = response.getString("local");
+                                sigla = response.getString("sigla");
+                            }
+                            alDialog(local, sigla);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+
+        });
+        mQueue.add(request);
+    }
+
+    private void alDialog(final String local, final String sigla) {
+        String title = "Escaneado com sucesso!";
+        String message = "Prosseguir com " + sigla;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        if (local == null || sigla == null) {
+            title = "Erro";
+            message = "O código não corresponde nenhum dado no banco.";
+            builder.setPositiveButton("Escanear novamente", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    scanCode();
+                }
+            }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+        } else {
+            builder.setPositiveButton("Prosseguir", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    changeAct(local, sigla);
+                }
+            }).setNeutralButton("Escanear novamente", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    scanCode();
+                }
+            }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+        }
+        builder.setTitle(title);
+        builder.setMessage(message);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void changeAct(String local, String sigla) {
+        Intent intent = new Intent(getApplicationContext(), Ticket.class);
+        intent.putExtra("MAQUINA_LOCAL", local);
+        intent.putExtra("MAQUINA_SIGLA", sigla);
+        intent.putExtra("USER_EMAIL", userEmail);
+        startActivity(intent);
     }
 }
--- SuportEtecia/app/src/main/java/com/example/suportetecia/Ticket.java
+++ SuportEtecia/app/src/main/java/com/example/suportetecia/Ticket.java
@@ -3,12 +3,33 @@ package com.example.suportetecia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Ticket extends AppCompatActivity {
+
+    String local;
+    String sigla;
+    String userEmail;
+
+    Button btnSendTicket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket);
+
+        local = getIntent().getStringExtra("MAQUINA_LOCAL");
+        sigla = getIntent().getStringExtra("MAQUINA_SIGLA");
+        userEmail = getIntent().getStringExtra("USER_EMAIL");
+
+        TextView tvLocal = findViewById(R.id.txtLocal);
+        tvLocal.setText(local);
+
+        TextView tvSigla = findViewById(R.id.txtSigla);
+        tvSigla.setText(sigla);
+
+        btnSendTicket = findViewById(R.id.btnSendTicket);
+//        btnSendTicket.setOnClickListener();
     }
 }
